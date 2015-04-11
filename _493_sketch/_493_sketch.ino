f64c8697b33ffd4d76e134fafeab4e806b9d4ef2
mehul <mgoyal3@illinois.edu>
2015-04-11 01:51:05 -0500

    incorporated mavlink messages. currently rotating the copter 180 degrees if an object is detected within 15 inches of any sensor

--- _493_sketch/_493_sketch.ino
+++ _493_sketch/_493_sketch.ino
@@ -1,47 +1,87 @@
-#include <mavlink.h>
+#include <math.h>
+
+#define 	cosf   cos
+#define 	sinf   sin
+#define 	tanf   tan
+#define 	fabsf   fabs
+#define 	fmodf   fmod
+#define 	cbrtf   cbrt
+#define 	hypotf   hypot
+#define 	squaref   square
+#define 	floorf   floor
+#define 	ceilf   ceil
+#define 	frexpf   frexp
+#define 	ldexpf   ldexp
+#define 	expf   exp
+#define 	coshf   cosh
+#define 	sinhf   sinh
+#define 	tanhf   tanh
+#define 	acosf   acos
+#define 	asinf   asin
+#define 	atanf   atan
+#define 	atan2f   atan2
+#define 	logf   log
+#define 	log10f   log10
+#define 	powf   pow
+#define 	isnanf   isnan
+#define 	isinff   isinf
+#define 	isfinitef   isfinite
+#define 	copysignf   copysign
+#define 	signbitf   signbit
+#define 	fdimf   fdim
+#define 	fmaf   fma
+#define 	fmaxf   fmax
+#define 	fminf   fmin
+#define 	truncf   trunc
+#define 	roundf   round
+#define 	lroundf   lround
+#define 	lrintf   lrint
+#define         sqrtf    sqrt
+#define MAV_COMP_ID_PRIMARY 1
+
+#include "/Users/mehulgoyal/Documents/cs493/arduino/libraries/mavlink/v1.0/ardupilotmega/mavlink.h"
 
 int trigPins[] = {50,48,46};
 int echoPins[] = {51,49,47};
 int numSensors = 3;
-int system_type = MAV_QUADROTOR;
-int autopilot_type = MAV_AUTOPILOT_PIXHAWK;
-int sysid = -1; // fix
-int compid = -1; // fix
-int target_sysid = -1; // fix
-int target_compid = -1; // fix
+int sysid = 252;
+int compid = 1;
+int target_sysid = 1;
+int target_compid = MAV_COMP_ID_PRIMARY;
 
 void setup() {
   Serial.begin(9600);
+  Serial1.begin(57600);
+  
+  for (int i = 0; i < 10; i++) {// tinker/change
+    sendArm();
+  }
 }
 
-void sendHeartbeat() {
-  mavlink_message_t msg;
-  uint8_t buf[MAVLINK_MAX_PACKET_LEN];
-  mavlink_msg_heartbeat_pack(sysid, compid, &msg, system_type, autopilot_type);
-  uint16_t len = mavlink_msg_to_send_buffer(buf, &msg);
-  // send message (uart ?)
+void sendArm() {
+  sendMavlinkMessage(MAV_CMD_COMPONENT_ARM_DISARM, 1.0, 0, 0, 0, 0, 0, 0);
 }
 
-void sendMavlinkMessage(int action) {
+void sendMavlinkMessage(int action, float param1, float param2, float param3, float param4, float param5, float param6, float param7) {
   mavlink_message_t msg;
   uint8_t buf[MAVLINK_MAX_PACKET_LEN];
-  mavlink_msg_action_pack(sysid, compid, &msg, target_sysid, target_compid, action);
+  mavlink_msg_command_long_pack(sysid, compid, &msg, target_sysid, target_compid, action, 1, param1, param2, param3, param4, param5, param6, param7);
   uint16_t len = mavlink_msg_to_send_buffer(buf, &msg);
-   // send message (uart ?)
+  Serial1.write(buf,len);
 }
 
 void loop() {
+  comm_receive();
   float dist=0;
   for(int i = 0; i< numSensors; i++) { 
       dist = getDistance(trigPins[i], echoPins[i]);
-      printDist(i, dist);
-      //delay for each individual sensor
+      if (dist < 15) { //tinker/change
+        // turn 180 degrees (relative to current yaw) at 5 degrees per second, clockwise
+        sendMavlinkMessage(MAV_CMD_CONDITION_YAW, 180.0, 5.0, 1, 1, 0, 0, 0);
+      }
       delay(50);
     }
-    Serial.println();
-    Serial.println();
-    Serial.println();
-    delay(1000);
+    delay(100);
 }
 
 void printDist(int sensor, float distance) {
@@ -79,4 +119,46 @@ float microsecondsToInches(float microseconds)
   // second). 
   // divide by 2 to get the distance of the obstacle.
   return microseconds / 74.0f / 2.0f;
+}
+
+void comm_receive() {
+ 
+       mavlink_message_t msg;
+	mavlink_status_t status;
+  
+	while(Serial1.available() > 0 ) 
+	{
+		uint8_t c = Serial1.read();
+		// Try to get a new message
+		if(mavlink_parse_char(MAVLINK_COMM_0, c, &msg, &status)) {
+			// Handle message
+                       
+			switch(msg.msgid)
+			{
+			        case MAVLINK_MSG_ID_HEARTBEAT:
+			        {
+                                    Serial.print(msg.msgid); 
+                                   Serial.println("heartbeat");  
+				  // E.g. read GCS heartbeat and go into
+                                  // comm lost mode if timer times out
+                                  break;
+			        }
+			        
+			case MAVLINK_MSG_ID_COMMAND_LONG:
+				// EXECUTE ACTION
+                                Serial.print(msg.msgid); 
+                                Serial.println("MESSAGE LONG"); 
+				break;
+			default:
+				//Do nothing
+                                Serial.print("default case ");
+                                Serial.print(msg.msgid);
+                                Serial.print(" ");                               
+                                Serial.println(MAVLINK_MSG_ID_COMMAND_LONG);
+				break;
+			}
+		}
+ 
+		// And get the next one
+	}
 }
